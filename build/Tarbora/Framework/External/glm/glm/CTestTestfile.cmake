0d95bc5fc4425a36c61ff041bbb525945d7a200a
SolansPuig <roger@solanspuig.cat>
2020-03-03 11:49:07 +0100

    removed build folder

--- build/Tarbora/Framework/External/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/roger/Workspace/Tarbora/Tarbora/Framework/External/glm/glm
-# Build directory: /home/roger/Workspace/Tarbora/build/Tarbora/Framework/External/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
